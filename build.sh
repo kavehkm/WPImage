adbf66702d1fc999a8b3b3143c98529104dc829a
kavehkm <mehrbaniankaveh@gmail.com>
2022-10-23 14:11:14 +0330

    update confs/www.conf: 1) change pm value into ondemand update build.sh: 1) update build tags

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker build -f 7.4/Dockerfile -t kavehkm/wp6:php7.4-fpm .
+docker build -f 7.4/Dockerfile -t kavehkm/wp6:php7.4-fpm-ondemand .
 
-docker build -f 8.1/Dockerfile -t kavehkm/wp6:php8.1-fpm .
+docker build -f 8.1/Dockerfile -t kavehkm/wp6:php8.1-fpm-ondemand .
